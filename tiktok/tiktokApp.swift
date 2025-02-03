322e3d790aee5827b3a6ddf3951109e7708970d4
tg-gt <tggauntlet@gmail.com>
2025-02-03 13:02:13 -0800

    Initial Commit

---
+++ tiktok/tiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokApp.swift
+//  tiktok
+//
+//  Created by gt on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct tiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
